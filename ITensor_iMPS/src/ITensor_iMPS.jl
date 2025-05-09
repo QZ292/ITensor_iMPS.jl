f13600128769d862a201f347c1861a3c8234c62c
QZ Zhu <123799445+QZ292@users.noreply.github.com>
2025-05-09 17:08:36 +0900

    Update ITensor_iMPS.jl

--- ITensor_iMPS/src/ITensor_iMPS.jl
+++ ITensor_iMPS/src/ITensor_iMPS.jl
@@ -6,5 +6,5 @@ include("types.jl")
 include("methods_canonical.jl")
 include("methods_biortho.jl")
 include("methods_check.jl")
-export iMPS_canonical, biorthonormal_initialize, canonical_initialize, canonicalize, biorthonormalize, checkimps
+export iMPS_canonical, biorthonormal_initialize, canonical_initialize, canonical_gauge, canonicalize, biorthonormalize, checkimps
 end # module iTensor_Biorthonormal_iMPS
